0e43d95877384b4a8316bef9303c28d22afaf8ad
Knoah Cotto <cotto.knoah@gmail.com>
2020-06-30 09:14:59 -0500

    exercise almost complete

--- src/main/java/exercise.java
+++ src/main/java/exercise.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
     public class exercise {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
-            System.out.println("Enter a number: ");
+            System.out.println("Please enter something: ");
             String userInput = sc.nextLine();
-//            System.out.println(userInput);
+            System.out.println("You entered: \"" + something + "\"");
+            System.out.println("\" + something + "\" " + )
+            System.out.println("Flipped Case: ") + StringUtils.swapCase(something);;
+            System.out.println("Reversed: " + StringUtils.reverse(something));
             System.out.println(StringUtils.isNumeric(userInput));
         }
 }
