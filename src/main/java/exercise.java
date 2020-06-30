c2db2470d08d45afec93510121b48c7be5aa3e15
Knoah Cotto <cotto.knoah@gmail.com>
2020-06-30 09:16:37 -0500

    update

--- src/main/java/exercise.java
+++ src/main/java/exercise.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
             Scanner sc = new Scanner(System.in);
             System.out.println("Please enter something: ");
             String userInput = sc.nextLine();
-            System.out.println("You entered: \"" + something + "\"");
-            System.out.println("\" + something + "\" " + )
-            System.out.println("Flipped Case: ") + StringUtils.swapCase(something);;
-            System.out.println("Reversed: " + StringUtils.reverse(something));
+//            System.out.println("You entered: \"" + something + "\"");
+//            System.out.println("\" + something + \" " + returnNumMessage(something));
+//            System.out.println("Flipped Case: ") + StringUtils.swapCase(something);;
+//            System.out.println("Reversed: " + StringUtils.reverse(something));
             System.out.println(StringUtils.isNumeric(userInput));
         }
 }
